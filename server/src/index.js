e513767f6e019f2adff20ee8a8eede955199bf13
double-hope <nadia.prohorchuk@gmail.com>
2022-07-26 19:53:16 +0300

    database

--- server/src/index.js
+++ server/src/index.js
@@ -1,2 +1,20 @@
 const { createServer } = require('graphql-yoga');
+const Query = require('./resolvers/Query');
+const { readFileSync } = require('fs');
 
+const resolvers = {
+    Query
+};
+
+const typeDefs = readFileSync(require.resolve('./schema.graphql')).toString('utf-8');
+
+const server = new createServer({
+    schema:{
+        typeDefs,
+        resolvers
+    }
+});
+
+server.start(() => {
+    console.log('http://localhost:4000/')
+})
---
+++ server/src/resolvers/Query.js
@@ -0,0 +1,15 @@
+function products(parent, args, context){
+    return [{
+        id: '1',
+        title: 'Product1',
+        price: 10.5
+    }, {
+        id: '2',
+        title: 'Product2',
+        price: 15.59
+    }];
+}
+
+module.exports = {
+    products
+}
