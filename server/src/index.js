b0f3fffdc59b2e3f8f11fcad14a7c9810367787f
double-hope <nadia.prohorchuk@gmail.com>
2022-07-26 21:17:51 +0300

    remove prisma

--- server/src/index.js
+++ server/src/index.js
@@ -2,22 +2,19 @@ const { createServer } = require('graphql-yoga');
 const Query = require('./resolvers/Query');
 const Mutation = require('./resolvers/Mutation');
 const { readFileSync } = require('fs');
-const { PrismaClient } = require('./generated/prisma-client-js')
 
 const resolvers = {
     Query,
-    Mutation,
+    //Mutation,
 };
 
 const typeDefs = readFileSync(require.resolve('./schema.graphql')).toString('utf-8');
-const prisma = new PrismaClient();
 
 const server = new createServer({
     schema:{
         typeDefs,
         resolvers
-    },
-    context: { prisma }
+    }
 });
 
 server.start(() => {
--- server/src/resolvers/Query.js
+++ server/src/resolvers/Query.js
@@ -1,5 +1,13 @@
-async function products(parent, args, context){
-    return await context.prisma.products();
+function products(parent, args, context){
+    return [{
+        id: '1',
+        title: 'Product1',
+        price: 10.5
+    }, {
+        id: '2',
+        title: 'Product2',
+        price: 15.59
+    }];
 }
 
 module.exports = {
